2bb85ba7323a6fabbf43e5a473a20d8b85b57947
Alexander Krivonosov <akrivonosov@provectus.com>
2021-12-12 11:50:34 +0300

    Add .env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import preact from "@preact/preset-vite";
 
-export default defineConfig({
-  plugins: [preact()],
-  esbuild: {
-    jsxFactory: 'h',
-    jsxFragment: 'Fragment'
-  }
-});
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  return defineConfig({
+    plugins: [preact()],
+    esbuild: {
+      jsxFactory: 'h',
+      jsxFragment: 'Fragment'
+    },
+    server: {
+      port: parseInt(process.env.VITE_PORT)
+    }
+  });
+
+}
